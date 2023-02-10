0c0a4e648b3f6b7adaaf71030198886e13ceb0c3
Leyre <huertasleyremontero@gmail.com>
2023-02-10 10:31:07 +0100

    day1, day2,day3

---
+++ day_2/variables.py
@@ -0,0 +1,72 @@
+print('#Day 2: 30 Days of python programming')
+# Variables in Python
+first_name = 'Leyre'
+last_name = 'Montero'
+full_name = 'Leyre  Montero'
+country = 'Spain'
+city = 'Jerez'
+age = '16'
+year = '2023'
+is_married = True
+is_true = False
+is_light_on = True
+on, one, line = True, 'Pepe', 5
+print('first_name, last_name, country, city, age, year is_married')
+print('first name:', first_name )
+print('last name:', last_name )
+print('country: ', country)
+print('age: ',age)
+print('married: ',is_married)
+print('is True', is_true)
+print(type, first_name)
+print(type, last_name)
+print(type, full_name)
+print(type, country)
+print(type, city)
+print(type, age)
+print(type, year)
+print(type, is_married)
+print(type, is_true)
+print(type, is_light_on )
+print('la longitud de mi nombre es', len(first_name))
+len('leyre')==len('leyre montero')
+
+num_one=5
+num_two=4
+otal = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+divide = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print('total')
+print(diff)
+print(product)
+print(divide)
+print(remainder)
+print(exp)
+print(floor_division)
+
+# Calculating the area and circumference of a circle
+radius = int(input("Give me radius of a circle: "))
+square_of_radius = radius * radius
+pi = 3.14
+area_of_circle = pi * square_of_radius
+print(area_of_circle)
+
+circum_of_circle = 2 * pi * radius
+print(circum_of_circle)
+
+# Getting inputs from users
+firstName = input("what is your first name? ")
+lastName = input("what is your last name? ")
+your_country = input("what country are you from? ")
+isAge = input("how old are you? ")
+
+print("Hello Sir, my name is", firstName, lastName, "I am from", your_country, "and I am", isAge, "years old.")
+
+
+
+
---
+++ day_3/operators.py
@@ -0,0 +1,14 @@
+#  Day 03: 30 days of python programming
+
+my_age = 16
+my_height = 1.53
+num_complex = 2 + 2j
+
+#Scrip that prompts the user to enter side a;side b, and side c of the triangle. Calculate the perimeter of the triangle(perimeter= a + b + c).
+#Stores the user input as an integer in the 'sideA' variable
+sideA = int(input("what's the base of the triangle")) 
+#Stores the user input as an integer in the 'sideA' variable
+sideB = int(input("what's the lenght of the side B of the triangle"))
+#Stores the user input as an integer in the 'sideA' variable
+sideC = int(input("what's the lenght of the side B of the triangle"))
+
