9a7346c9051e4df57ce9e8e5a5e97a5591640a02
Leyre <huertasleyremontero@gmail.com>
2023-03-03 10:43:09 +0100

    day5

---
+++ day_7/sets.py
@@ -0,0 +1,6 @@
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+st={'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+len(it_companies)
+it_companies.add('Twitter')
+it_companies= {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+it_companies= ('')
